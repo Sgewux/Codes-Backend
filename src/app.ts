5e3510087d7c0c428041372983c8601be8548c42
cmottao <cristianstevenmottaojeda@gmail.com>
2025-01-03 21:29:09 -0500

    Create project structure and install required dependencies

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+export default app;
---
+++ src/db.ts
@@ -0,0 +1,15 @@
+import mysql from "mysql2/promise";
+
+export async function getConnection() {
+  try {
+    return await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || "3306"),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+  } catch (e) {
+    throw e;
+  }
+}
