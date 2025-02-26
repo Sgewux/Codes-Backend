17e1bd86d2a0ce0680d4e9a34a6ab868c47aa727
Jestebansamt <jestebansant@gmail.com>
2025-02-26 17:26:13 -0500

    added CRUD

--- main/src/routes/CRUD.routes.ts
+++ main/src/routes/CRUD.routes.ts
@@ -3,7 +3,7 @@ import callProcedure from "../libs/callProcedure";
 
 const router = Router();
 
-router.post("/problems", async (req: Request, res: Response) => {
+router.post("/create-problems", async (req: Request, res: Response) => {
   const { name,statement,editorial,time_limit_seconds,memory_limit_mb,problemsetter_handle,input,output} = req.body;
   
   if (!name || !statement || !editorial || !time_limit_seconds || !memory_limit_mb || !problemsetter_handle || !input || !output) {
@@ -18,5 +18,52 @@ router.post("/problems", async (req: Request, res: Response) => {
     res.status(500).json({ message: e.message });
   }
 });
+
+
+router.get("/:handle/read-problem", async (req: Request, res: Response) => {
+  const { handle } = req.params;
+  try {
+    const data = await callProcedure("sp_read_problem_by_handle", [handle]);
+
+    const problems = data[0]; 
+
+    res.json({ problems });
+  } catch (e: any) {
+    console.error(e);
+    res.status(500).json({ success: false, message: e.message });
+  }
+});
+
+router.put("/update-problem/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { statement, editorial } = req.body;
+
+  if (!statement && !editorial) {
+    res.status(400).json({ success: false, message: "No hay cambios para actualizar." });
+  }
+
+  try {
+    await callProcedure("sp_update_problem", [id, statement, editorial]);
+
+    res.json({ success: true, message: "Problema actualizado correctamente" });
+  } catch (e: any) {
+    console.error(e);
+    res.status(500).json({ success: false, message: e.message });
+  }
+});
+
+
+router.delete("/delete-problem/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    await callProcedure("sp_delete_problem", [Number(id)]);
+    res.json({ success: true, message: "Problema eliminado correctamente" });
+  } catch (e: any) {
+    console.error(e);
+    res.status(500).json({ success: false, message: e.message });
+  }
+});
+
 
 export default router;
