0e04a8bd0ef81802015a8a45259c30f8c58a0684
Jestebansamt <jestebansant@gmail.com>
2025-02-28 19:55:50 -0500

    CRUD

--- main/src/routes/CRUD.routes.ts
+++ main/src/routes/CRUD.routes.ts
@@ -3,7 +3,22 @@ import callProcedure from "../libs/callProcedure";
 
 const router = Router();
 
-router.post("/problems", async (req: Request, res: Response) => {
+router.get("/problems/problemsetter/:handle", async (req: Request, res: Response) => {
+  const { handle } = req.params;
+  try {
+    const data = await callProcedure("sp_read_problem_by_problemsetter_handle", [handle]);
+
+    const problems = data[0];
+
+    res.json({ problems });
+  } catch (e: any) {
+    console.error(e);
+    res.status(500).json({ success: false, message: e.message });
+  }
+});
+
+
+router.post("/problems/problemsetter/", async (req: Request, res: Response) => {
   const { name, statement, editorial, time_limit_seconds, memory_limit_mb, problemsetter_handle, input, output } = req.body;
 
   if (!name || !statement || !editorial || !time_limit_seconds || !memory_limit_mb || !problemsetter_handle || !input || !output) {
@@ -20,21 +35,7 @@ router.post("/problems", async (req: Request, res: Response) => {
 });
 
 
-router.get("/:handle/read-problem", async (req: Request, res: Response) => {
-  const { handle } = req.params;
-  try {
-    const data = await callProcedure("sp_read_problem_by_handle", [handle]);
-
-    const problems = data[0];
-
-    res.json({ problems });
-  } catch (e: any) {
-    console.error(e);
-    res.status(500).json({ success: false, message: e.message });
-  }
-});
-
-router.put("/update-problem/:id", async (req: Request, res: Response) => {
+router.put("/problems/problemsetter/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   const { statement, editorial } = req.body;
 
@@ -53,9 +54,8 @@ router.put("/update-problem/:id", async (req: Request, res: Response) => {
 });
 
 
-router.delete("/delete-problem/:id", async (req: Request, res: Response) => {
+router.delete("/problems/problemsetter/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
-
   try {
     await callProcedure("sp_delete_problem", [Number(id)]);
     res.json({ success: true, message: "OK" });
@@ -64,6 +64,7 @@ router.delete("/delete-problem/:id", async (req: Request, res: Response) => {
     res.status(500).json({ success: false, message: e.message });
   }
 });
+
 
 
 export default router;
