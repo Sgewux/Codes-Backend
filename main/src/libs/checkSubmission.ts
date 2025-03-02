5fcb011fabd3587bf56d5cec3548ad61aed577b8
cmottao <cristianstevenmottaojeda@gmail.com>
2025-03-01 19:49:58 -0500

    Added evaluate submission functionality

---
+++ main/src/libs/checkSubmission.ts
@@ -0,0 +1,61 @@
+import dotenv from "dotenv";
+import callProcedure from "./callProcedure";
+import { runCode } from "./CallRunnerService";
+import { SubmissionResult } from "../types/submissions";
+import { RowDataPacket } from "mysql2";
+
+dotenv.config();
+
+async function checkSubmission(submission_id: number, problem_id: number, code: string, time_limit: number): Promise<SubmissionResult> {
+  try {
+    const result = await callProcedure("get_problem_tests", [problem_id]);
+    const rows: RowDataPacket[] = result[0] as RowDataPacket[];
+
+    let executionTime: number = 0;
+
+    for (const test of rows) {
+      const { input, output: expectedOutput } = test;
+      const { data } = await runCode(submission_id, code, input, time_limit);
+      executionTime = Math.max(executionTime, Number(data.execution_time));
+
+      if (data.status == "COMPILATION_ERROR") {
+        return {
+          verdict: "CE",
+          execution_time: 0,
+          log: data.log
+        };
+      }
+
+      if (data.status == "TIME_LIMIT_EXCEEDED") {
+        return {
+          verdict: "TL",
+          execution_time: time_limit
+        };
+      }
+
+      if (data.status == "RUNTIME_ERROR") {
+        return {
+          verdict: "RT",
+          execution_time: 0,
+          log: data.log
+        };
+      }
+
+      if (data.output?.trim() !== expectedOutput.trim()) {
+        return {
+          verdict: "WA",
+          execution_time: executionTime
+        };
+      }
+      console.log(executionTime);
+    }
+    return {
+      verdict: "AC",
+      execution_time: executionTime,
+    };
+  } catch (e) {
+    throw new Error("Submission checking failed");
+  }
+}
+
+export default checkSubmission;
--- main/src/routes/submissions.routes.ts
+++ main/src/routes/submissions.routes.ts
@@ -1,23 +1,52 @@
 import { Router, Request, Response } from "express";
 import callProcedure from "../libs/callProcedure";
+import { checkAuth } from "../middlewares/auth.middlewares";
+import checkSubmission from "../libs/checkSubmission";
+import { SubmissionResult } from "../types/submissions";
 
 const router = Router();
 
-router.get("/submissions/:id", async (req: Request, res: Response) => {
+router.get("/submissions/:id", async (req: Request, res: Response): Promise<void> => {
   const { id } = req.params;
 
   try {
-    const [result, _] = await callProcedure("get_submission_by_id", [id]);
-    
-    if(result[0]){
-      res.json(result[0]);
-    } else {
-      res.status(404).json({"message": `No submission with id: ${id}`});
-    }
+    const [result] = await callProcedure("get_submission_by_id", [id]);
 
-  } catch (e) {
-    console.log(e);
-    res.status(500).json();
+    if (!result[0]) {
+      res.status(400).json({ message: `No submission with id: ${id}` });
+      return;
+    }
+    res.json(result[0]);
+
+  } catch (e: any) {
+    console.log(e.message);
+    res.status(500).json({ message: e.message });
+  }
+});
+
+router.post("/submissions", checkAuth(["contestant"]), async (req: Request, res: Response): Promise<void> => {
+  const { problem_id, code } = req.body;
+  const handle = req.user?.handle;
+
+  if (!problem_id || !code) {
+    res.status(400).json({ message: "Missing required fields" });
+    return;
+  }
+
+  try {
+    const [result] = await callProcedure("create_submission", [handle, problem_id, code]);
+    const submissionId = result[0]?.submission_id;
+
+    const [result1] = await callProcedure("get_problem_time_limit", [problem_id]);
+    const time_limit = result1[0].time_limit_seconds;
+
+    const verdict: SubmissionResult = await checkSubmission(submissionId, problem_id, code, time_limit);
+    await callProcedure("update_submission_verdict", [submissionId, verdict.verdict, verdict.execution_time]);
+
+    res.status(201).json({ message: "Submission created", submissionId });
+  } catch (e: any) {
+    console.log(e.message);
+    res.status(500).json({ message: e.message });
   }
 });
 
